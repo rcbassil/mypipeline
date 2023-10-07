8d8e0e38770a62ee37365b19e27633d1448ed444
Rodrigo Bassil <rcbassil@gmail.com>
2023-10-07 15:33:56 -0400

    Reorg

---
+++ building-blocks/jobFuncs.groovy
@@ -0,0 +1,16 @@
+import hudson.console.*
+
+def init(){
+    // Prepopulate the array with the default value, which is used if no YAML selected
+    yamlsList = ['env-variable']
+    dir('profiles') {
+        files = findFiles(glob: '*.yaml')
+    }
+    files.each { file ->
+        yamlsList.add(file.name)
+    }
+    //checkRequiredParams()
+    //if(SkipAllStages) {
+    //    setBuildTitle("Parameter Initialization")
+    //}
+}
