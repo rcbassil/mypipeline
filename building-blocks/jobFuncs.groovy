5de475070bd7e9d56761218521fdb2c7f8cf21a5
Rodrigo Bassil <rcbassil@gmail.com>
2023-10-07 20:23:18 -0400

    Adding jobFuncs

--- building-blocks/jobFuncs.groovy
+++ building-blocks/jobFuncs.groovy
@@ -16,4 +16,61 @@ def init(){
     //}
 }
 
+
+def ReadConfig(){
+    // use DeployYaml parameter value, if it's not default env-variable
+    if (params.DeployYaml != "env-variable"){
+        env.yamlstouse = params.DeployYaml
+    }
+    //selectStagesToRun()
+    yamls = readYaml file: "profiles/"+env.yamlstouse
+    addFileToPathMap(env.yamlstouse, "profiles/"+env.yamlstouse)
+    addDataToConfig(yamls)
+    printJobConfig()
+    // save jobConfig as JSON in the ENV variable,
+    // to get access from shell scripts (using common.sh get_job_config function)
+    env.jobConfig = groovy.json.JsonOutput.toJson(jobConfig);        
+    setBuildTitle(env.yamlstouse)
+    //storeGitCredentials()
+
+    //addStageToStagesRan("Read Config")
+}
+
+
+def addFileToPathMap(fName, fPath){
+    String prefix = 'https://github.com/rcbassil/mypipeline/blob/'
+    String branch = scm.branches[0].name
+    branch = branch.replace("*/", "").trim() + '/'
+    fullYamlPath[fName] = prefix + branch + fPath
+}
+
+def addDataToConfig(data){
+    data.each { key, value ->
+        jobConfig[key] = value
+    }
+}
+
+def printJobConfig(){
+    String outputStr = "Deployment Yaml: ${printYamlLink(env.yamlstouse)}\njobConfig:\n"
+    jobConfig.each { key, value ->
+        outputStr += "  ${key}: ${printYamlLink(value)}\n"
+    }
+    println outputStr
+}
+
+def setBuildTitle(title){
+
+    // add userName
+    //if(!SkipAllStages){
+        def userCause = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')
+        //if(isManuallyStartedBuild()){            
+            title += " "+userCause.userName
+       // }else{
+       //     title += " (timer)"
+       // }
+    }
+    currentBuild.displayName = "#${BUILD_NUMBER}: " + title
+}
+
+
 return this
