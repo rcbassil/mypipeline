8a1fa1db5882c2d1df0d8a4de92620c4adc95863
rcbassil <rcbassil@gmail.com>
2023-10-07 20:39:48 -0400

    New test (#24)
    * Testing build
    
    * Testing build
    
    * Testing build
    
    * Fix pipeline
    
    * Fix pipeline
    
    * Fix pipeline
    
    * Fix pipeline
    
    * Fix pipeline
    
    * Fix pipeline
    
    * Fix pipeline
    
    * Fix pipeline
    
    * Fix pipeline
    
    * Fix pipeline
    
    * Fix pipeline
    
    * Fix pipeline
    
    * Fix pipeline
    
    * Publishing port
    
    * Publishing port
    
    * Adding config.yaml
    
    * Adding config.yaml
    
    * Adding config.yaml
    
    * Adding parameters
    
    * Adding config.yaml
    
    * Adding config.yaml
    
    * Get params stage
    
    * Get params stage
    
    * Get params stage
    
    * Get params stage
    
    * Get params stage
    
    * vault stage
    
    * vault stage
    
    * vault stage
    
    * vault stage
    
    * vault stage
    
    * Reorg
    
    * Reorg
    
    * Reorg
    
    * Reorg
    
    * Reorg
    
    * Reorg
    
    * Adding jobFuncs
    
    * Adding jobFuncs
    
    * Adding jobFuncs
    
    * Adding jobFuncs
    
    * Adding jobFuncs

---
+++ building-blocks/jobFuncs.groovy
@@ -0,0 +1,93 @@
+import hudson.console.*
+
+def init(){
+    // Prepopulate the array with the default value, which is used if no YAML selected
+    yamlsList = ['env-variable']
+    echo "Test"
+    dir('profiles') {
+        files = findFiles(glob: '*.yaml')
+    }
+    files.each { file ->
+        yamlsList.add(file.name)
+    }
+    //checkRequiredParams()
+    //if(SkipAllStages) {
+    //    setBuildTitle("Parameter Initialization")
+    //}
+}
+
+fullYamlPath = [:]
+
+// Holds the deployment configuration data
+jobConfig = [:]
+
+
+
+def ReadConfig(){
+    // use DeployYaml parameter value, if it's not default env-variable
+    if (params.DeployYaml != "env-variable"){
+        env.yamlstouse = params.DeployYaml
+    }
+    //selectStagesToRun()
+    yamls = readYaml file: "profiles/"+env.yamlstouse
+    addFileToPathMap(env.yamlstouse, "profiles/"+env.yamlstouse)
+    addDataToConfig(yamls)
+    printJobConfig()
+    // save jobConfig as JSON in the ENV variable,
+    // to get access from shell scripts (using common.sh get_job_config function)
+    env.jobConfig = groovy.json.JsonOutput.toJson(jobConfig);        
+    setBuildTitle(env.yamlstouse)
+    //storeGitCredentials()
+
+    //addStageToStagesRan("Read Config")
+}
+
+
+def addFileToPathMap(fName, fPath){
+    String prefix = 'https://github.com/rcbassil/mypipeline/blob/'
+    String branch = scm.branches[0].name
+    branch = branch.replace("*/", "").trim() + '/'
+    fullYamlPath[fName] = prefix + branch + fPath
+}
+
+def addDataToConfig(data){
+    data.each { key, value ->
+        jobConfig[key] = value
+    }
+}
+
+def printJobConfig(){
+    String outputStr = "Deployment Yaml: ${printYamlLink(env.yamlstouse)}\njobConfig:\n"
+    jobConfig.each { key, value ->
+        outputStr += "  ${key}: ${printYamlLink(value)}\n"
+    }
+    println outputStr
+}
+
+String getHyperlink(String url, String text) {
+    return hudson.console.ModelHyperlinkNote.encodeTo(url, text)
+}
+
+def printYamlLink(value){
+    if("${value}".endsWith(".yaml")){
+        value = getHyperlink(fullYamlPath[value], value)
+    }
+    return value
+}
+
+def setBuildTitle(title){
+
+    // add userName
+    //if(!SkipAllStages){
+        def userCause = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')
+        //if(isManuallyStartedBuild()){            
+            title += " "+userCause.userName
+       // }else{
+       //     title += " (timer)"
+       // }
+    //}
+    currentBuild.displayName = "#${BUILD_NUMBER}: " + title
+}
+
+
+return this
