980e528a0c10ba2bd6ff8d1314bb1894ac1a9003
Rodrigo Bassil <rcbassil@gmail.com>
2023-10-09 11:12:11 -0400

    Adding jobFuncs

--- building-blocks/jobFuncs.groovy
+++ building-blocks/jobFuncs.groovy
@@ -75,7 +75,7 @@ def withSecretEnv(List<Map> varAndPasswordList, String secretId, Closure closure
     script{
         echo "${secretId}"
         wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: varAndPasswordList]) {
-            it.password = sh(script: '''curl -s -H "X-Vault-Token: $VAULT_TOKEN" -X GET http://192.168.8.148:8200/v1/kv/data/dev-creds/mysecrets | jq -r '.data.data."''' + secretId + '''"' ''', returnStdout: true).trim()
+            vaultSecret = sh(script: '''curl -s -H "X-Vault-Token: $VAULT_TOKEN" -X GET http://192.168.8.148:8200/v1/kv/data/dev-creds/mysecrets | jq -r '.data.data."''' + secretId + '''"' ''', returnStdout: true).trim()
             withEnv(varAndPasswordList.collect { "${it.var}=${it.password}" }) {
             closure()
             }
