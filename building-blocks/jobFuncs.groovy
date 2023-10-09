bf8b27b184f5fecbb9698fd6c0b7123c33158b01
Rodrigo Bassil <rcbassil@gmail.com>
2023-10-09 11:46:13 -0400

    Adding jobFuncs

--- building-blocks/jobFuncs.groovy
+++ building-blocks/jobFuncs.groovy
@@ -64,11 +64,8 @@ def GetVaultSecrets(){
 def GetSecret(String secretId){
     //vSecret = GetSecretFromVault(secretId)
     withSecretEnv([[var: 'SECRET', password: 'GetSecretFromVault(secretId)']]) {
-        sh '''#!/bin/bash -eu
-    echo "with env use:      ${SECRET}"
-    '''
-    sleep 1
         echo "Outside SH: SECRET=${SECRET}"
+        echo "Outside SH: SECRET=$SECRET"
         //echo "Outside SH: vSecret"
     }
 }
