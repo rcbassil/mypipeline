a968bdd0a5f274741d0fbd54d5e36b37d6c26cba
Rodrigo Bassil <rcbassil@gmail.com>
2023-10-14 01:13:47 -0400

    Adding example stage

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env sh
 
+source ./trycatch.sh
+
+try
+(
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
 set -x
 kill $(cat .pidfile)
+)
+catch || {
+    case $exception_code in
+        *)
+            echo "Unknown error: $exit_code"
+            throw $exit_code    # re-throw an unhandled exception
+        ;;
+    esac
+}
